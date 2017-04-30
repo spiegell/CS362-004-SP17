28e828b19ffe94c98a9e48df1f6194bd324334f9
spiegell <spiegell@oregonstate.edu>
2017-04-30 15:24:37 -0700

    Added quiz 2

---
+++ projects/spiegell/quiz2/randomstring.c
@@ -0,0 +1,9 @@
+// Lauren Spiegel
+//Random Testing Quiz 2
+//Text randomstring.c
+
+
+
+/* The random tester was developed using rand(),  which returns a pseudo-random integral number between the range. The numbers generated will not be related. By doing this, the error message can be displayed. Char inputchar() uses the rand() function and returns the standard printable ASCII characters. The numbers range from a common 32 to 127 for 7 bit chars. The range has (high+1-low)+low elements. Char *inputstring() uses a string with the same numbers for ASCII characters. The memory was allocated for 5 characters. 
+After the first try, the program ran 622537 Iterations before finally reaching the “reset.” State 9 was reached at Iteration 1133.  The second run, the program created 2416702 iterations.  
+*/
